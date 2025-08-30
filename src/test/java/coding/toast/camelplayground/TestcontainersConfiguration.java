51f23399ada1a6b8ae9244ce8518616d5139ee3e
CodingToastBread <mickeymouset@naver.com>
2025-08-30 22:40:07 +0900

    first commit

---
+++ src/test/java/coding/toast/camelplayground/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package coding.toast.camelplayground;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
